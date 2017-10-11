2b732a9b4e2a0eec049ca6c4fedf97fe80e493ba
Basil <bsafwat@gmail.com>
2017-10-11 21:47:21 +0100

    Create Blank.vue

---
+++ Blank.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="blank">
+    <h1>blank</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  // global
+  name: "blank",
+  
+  // dependencies
+  components: {},
+  
+  // interface
+  props: {},
+  
+  // local state
+  data () {
+    return {}
+  },
+  computed: {},
+  
+  // lifecycle events
+  mounted() {},
+  
+  // non-reactive
+  methods: {}
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../assets/sass/variables";
+</style>
